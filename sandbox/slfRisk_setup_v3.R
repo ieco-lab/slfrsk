d92dfc833037ff5d9ea1a7cb5b87cc17a6929a85
Matthew Helmus <mrhelmus@temple.edu>
2020-08-31 07:17:35 -0400

    Initialized FOASTAT dependency
    FAOSTAT R script and Rmd to calculate wine market size.

---
+++ sandbox/slfRisk_setup_v3.R
@@ -0,0 +1,63 @@
+library(tidyverse)
+library(devtools)
+library(pkgdown)
+library(roxygen2)
+# library(fs)
+# library(usethis)
+
+# TODO
+
+#options(ENTREZ_KEY = ncbi_key)
+
+#pkgdown::build_articles()
+pkgdown::preview_site()
+pkgdown::build_site()
+
+load_all()
+document()
+check()
+build()
+install()
+
+use_package("dplyr", type = "Imports")
+use_package("stringr", type = "Imports") #switch to tidyverse?
+use_package("ggplot2", type = "Imports")
+use_package("here", type = "Imports") #added by NAH
+use_package("tools", type = "Imports") #added by NAH
+use_package("usethis", type = "Imports") #added by NAH
+
+
+
+#----
+# Vignettes
+
+usethis::use_vignette("vignette-010-tidy-data")
+usethis::use_vignette("vignette-020-quadrant-plots")
+usethis::use_vignette("vignette-030-risk-maps")
+usethis::use_vignette("vignette-040-market-plot")
+
+#----
+## ONLY RUN CAREFULLY
+
+# Functions
+
+use_r("function name")                      # Make functions
+use_r("function name")                      # Test functions
+use_testthat()
+use_test("function name")
+
+# Build
+
+use_package_doc()
+use_mit_license("iEcoLab")                  # License
+use_readme_rmd()                            # edit GitHub README here?
+rmarkdown::render("README.Rmd")             # or use "Knit HTML" in RStudio
+
+if (interactive()) {                        # maybe good or bad...
+  suppressMessages(require(devtools))
+}
+
+use_build_ignore("[.]ini$", escape = FALSE)
+use_build_ignore("sandbox", escape = FALSE)
+use_build_ignore("README.html", escape = FALSE)
+use_git_ignore("desktop.ini")
