ffc5f050fb8119215566742d28bf814d4c746a43
Nicholas Huron <nahuron@gmail.com>
2020-11-03 12:07:28 -0500

    revised new cor plot

--- sandbox/downsample_raster_models.R
+++ sandbox/downsample_raster_models.R
@@ -11,40 +11,75 @@ library(rgdal)
 library(raster)
 
 #set working directory
-setwd("/Volumes/GoogleDrive/My Drive/spotted_lanternfly_ieco_projects/")
+#setwd("/Volumes/GoogleDrive/My Drive/spotted_lanternfly_ieco_projects/")
+
+mypath <- "/Volumes/GoogleDrive/Shared drives/slfData/data/slfRisk"
+
 
 #################################
 #world
 #################################
 
 #read in a geotiff version of the maxent suitability for all three models
-enm_data_slftoh <- raster("./data/geotiff_enms/slftoh.tif")
-enm_data_toh <- raster("./data/geotiff_enms/toh.tif")
-enm_data_slf <- raster("./data/geotiff_enms/slf.tif")
+enm_data_slftoh <- raster(file.path(mypath, "maxent_models", "slftoh.tif"))
+enm_data_toh <- raster(file.path(mypath, "maxent_models", "toh.tif"))
+enm_data_slf <- raster(file.path(mypath, "maxent_models", "slf.tif"))
+
+enm_data_mean <- raster(file.path(mypath, "maxent_models", "slftoh_ensemble_mean.tif"))
 
 #stack the rasters
-enm_data <- stack(c(enm_data_slftoh, enm_data_slf, enm_data_toh))
+#enm_data <- stack(c(enm_data_slftoh, enm_data_slf, enm_data_toh))
 
 #make mean raster
-enm_data_mean <- mean(enm_data)
+#enm_data_mean <- mean(enm_data)
 
 #write out the resulting file
-writeRaster(x = enm_data_mean, filename = "./data/geotiff_enms/slftoh_ensemble_mean.tif", format = "GTiff")
+#writeRaster(x = enm_data_mean, filename = "./data/geotiff_enms/slftoh_ensemble_mean.tif", format = "GTiff")
 
 
-#downsample by a factor of 2
-enm_data_slftoh2 <- aggregate(enm_data_slftoh, fact = 2, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/slftoh_downsampled_x2.tif", overwrite = F)
-enm_data_slf2 <- aggregate(enm_data_slf, fact = 2, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/slf_downsampled_x2.tif", overwrite = F)
-enm_data_toh2 <- aggregate(enm_data_toh, fact = 2, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/toh_downsampled_x2.tif", overwrite = F)
+#downsample by a factor of 4
+enm_data_mean4 <- aggregate(enm_data_mean, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "slftoh_ensemble_mean_downsampled_x4.tif"), overwrite = F)
+enm_data_slftoh4 <- aggregate(enm_data_slftoh, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "slftoh_downsampled_x4.tif"), overwrite = F)
+enm_data_slf4 <- aggregate(enm_data_slf, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "slf_downsampled_x4.tif"), overwrite = F)
+enm_data_toh4 <- aggregate(enm_data_toh, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "toh_downsampled_x4.tif"), overwrite = F)
 
-#same by a factor of 4 from original
-enm_data_slftoh5 <- aggregate(enm_data_slftoh, fact = 5, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/slftoh_downsampled_x5.tif", overwrite = F)
-enm_data_slf5 <- aggregate(enm_data_slf, fact = 5, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/slf_downsampled_x5.tif", overwrite = F)
-enm_data_toh5 <- aggregate(enm_data_toh, fact = 5, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/toh_downsampled_x5.tif", overwrite = F)
+#same by a factor of 10 from original
+enm_data_mean10 <- aggregate(enm_data_mean, fact = 10, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "slftoh_ensemble_mean_downsampled_x10.tif"), overwrite = F)
+enm_data_slftoh10 <- aggregate(enm_data_slftoh, fact = 10, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "slftoh_downsampled_x10.tif"), overwrite = F)
+enm_data_slf10 <- aggregate(enm_data_slf, fact = 10, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "slf_downsampled_x10.tif"), overwrite = F)
+enm_data_toh10 <- aggregate(enm_data_toh, fact = 10, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "toh_downsampled_x10.tif"), overwrite = F)
 
 #################################
 #USA (lower 48)
 #################################
+
+
+#get US from world data
+world <- readOGR(file.path(mypath, "geo_shapefiles/gadm36_levels_shp/gadm36_0.shp"), verbose = F, p4s = '+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0')
+
+#isolate US
+usa <- world[world@data$NAME_0 == "United States",]
+
+#crop the models to US
+usa_enm_mean <- enm_data_mean %>%
+  crop(x = ., y = usa) %>%
+  mask(x = ., mask = usa)
+usa_enm_slftoh <- enm_data_slftoh %>%
+  crop(x = ., y = usa) %>%
+  mask(x = ., mask = usa)
+usa_enm_toh <- enm_data_toh %>%
+  crop(x = ., y = usa) %>%
+  mask(x = ., mask = usa)
+usa_enm_slf <- enm_data_slf %>%
+  crop(x = ., y = usa) %>%
+  mask(x = ., mask = usa)
+
+#write out results
+writeRaster(x = usa_enm_mean, file.path(mypath, "maxent_models", "slftoh_ensemble_mean_usa.tif"), format = "GTiff")
+writeRaster(x = usa_enm_slftoh, filename = file.path(mypath, "maxent_models", "slftoh_usa.tif"), format = "GTiff")
+writeRaster(x = usa_enm_toh, filename = file.path(mypath, "maxent_models", "toh_usa.tif"), format = "GTiff")
+writeRaster(x = usa_enm_slf, filename = file.path(mypath, "maxent_models", "slf_usa.tif"), format = "GTiff")
+
 
 #read in USA clipped models (clipping is from geodata.R)
 enm_data_slftoh_usa <- raster("./data/geotiff_enms/slftoh_usa.tif")
@@ -52,48 +87,9 @@ enm_data_toh_usa <- raster("./data/geotiff_enms/toh_usa.tif")
 enm_data_slf_usa <- raster("./data/geotiff_enms/slf_usa.tif")
 
 
-#downsample by a factor of 2
-enm_data_slftoh_usa2 <- aggregate(enm_data_slftoh_usa, fact = 2, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/slftoh_usa_downsampled_x2.tif", overwrite = F)
-enm_data_slf_usa2 <- aggregate(enm_data_slf_usa, fact = 2, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/slf_usa_downsampled_x2.tif", overwrite = F)
-enm_data_toh_usa2 <- aggregate(enm_data_toh_usa, fact = 2, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/toh_usa_downsampled_x2.tif", overwrite = F)
-
 #downsample by a factor of 4
-enm_data_slftoh_usa4 <- aggregate(enm_data_slftoh_usa, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/slftoh_usa_downsampled_x4.tif", overwrite = F)
-enm_data_slf_usa4 <- aggregate(enm_data_slf_usa, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/slf_usa_downsampled_x4.tif", overwrite = F)
-enm_data_toh_usa4 <- aggregate(enm_data_toh_usa, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = "./data/geotiff_enms/toh_usa_downsampled_x4.tif", overwrite = F)
-
-
-#################################
-#now re-read in the downsampled rasters to create the mean model
-#################################
-
-#USA
-enm_data_slftoh_usa4 <- raster("./data/geotiff_enms/slftoh_usa_downsampled_x4.tif")
-enm_data_slf_usa4 <- raster("./data/geotiff_enms/slf_usa_downsampled_x4.tif")
-enm_data_toh_usa4 <- raster("./data/geotiff_enms/toh_usa_downsampled_x4.tif")
-
-#stack the rasters
-enm_data_usa <- stack(c(enm_data_slftoh_usa4, enm_data_slf_usa4, enm_data_toh_usa4))
-#crop to continental USA
-enm_data_usa <- raster::crop(enm_data_usa, extent(-124.7628, -66.94889, 24.52042, 49.3833))
-
-#make mean raster
-enm_data_usa_mean <- mean(enm_data_usa)
-
-#write out the resulting file
-writeRaster(x = enm_data_usa_mean, filename = "./data/geotiff_enms/slftoh_usa_downsampled_x4_mean.tif", format = "GTiff")
-
-#WORLD
-enm_data_slftoh4 <- raster("./data/geotiff_enms/slftoh_downsampled_x4.tif")
-enm_data_slf4 <- raster("./data/geotiff_enms/slf_downsampled_x4.tif")
-enm_data_toh4 <- raster("./data/geotiff_enms/toh_downsampled_x4.tif")
-
-#stack the rasters
-enm_data <- stack(c(enm_data_slftoh4, enm_data_slf4, enm_data_toh4))
-
-#make mean raster
-enm_data_mean <- mean(enm_data)
-
-#write out the resulting file
-writeRaster(x = enm_data_mean, filename = "./data/geotiff_enms/slftoh_downsampled_x4_mean.tif", format = "GTiff")
+enm_data_mean_usa4 <- aggregate(usa_enm_mean, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "slftoh_ensemble_mean_downsampled_x4_usa.tif"), overwrite = F)
+enm_data_slftoh_usa4 <- aggregate(usa_enm_slftoh, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "slftoh_downsampled_x4_usa.tif"), overwrite = F)
+enm_data_toh_usa4 <- aggregate(usa_enm_toh, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "toh_downsampled_x4_usa.tif"), overwrite = F)
+enm_data_slf_usa4 <- aggregate(usa_enm_slf, fact = 4, fun = mean, expand = TRUE, na.rm = TRUE, filename = file.path(mypath, "maxent_models", "slf_downsampled_x4_usa.tif"), overwrite = F)
 
